d20111f88889f23668d4c6d3b8d95490c02c937c
Anna Sours <acsours@gmail.com>
2021-04-16 15:36:16 -0600

    reads json and plots 1 marker

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 //create map
 var myMap=L.map('map', {
-    center: [37.09, -95.71],
-    zoom: 5
+    center: [15.5994, -28.6731],
+    zoom: 3
 });
 //create a tile layer
 
@@ -17,3 +17,18 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 //add url
 earthquake_url='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
 //load geojson
+d3.json(earthquake_url).then(function(data){
+    // console.log(data['features'][0]['geometry']['coordinates'])
+    var one_earthquake = data['features'][0]
+    // console.log(one_earthquake)
+    var coordinates = one_earthquake['geometry']['coordinates']
+    console.log(coordinates)
+    // put a marker on the map at the lat long of this earthquake
+    L.circle([coordinates[1],coordinates[0]], {
+        fillOpacity: 0.75,
+        color: "blue", 
+        fillColor: 'red',
+        radius: 100000
+      }).bindPopup("<h3>" + one_earthquake['properties']['place']+"</h3>").addTo(myMap)
+
+});
